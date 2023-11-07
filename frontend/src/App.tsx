8e3db821f98515e787066bbdbf29e6eca237ad1e
Onizuka893 <92974194+Onizuka893@users.noreply.github.com>
2023-11-07 19:46:43 +0100

    Import frontend content

---
+++ frontend/src/App.tsx
@@ -0,0 +1,56 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import React from 'react';
+import { Header } from './Header';
+import { HomePage } from './HomePage';
+import { fontFamily, fontSize, gray2 } from './Styles';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
+import { NotFoundPage } from './NotFoundPage';
+import { QuestionPage } from './QuestionPage';
+const AskPage = React.lazy(() => import('./AskPage'));
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route
+            path="ask"
+            element={
+              <React.Suspense
+                fallback={
+                  <div
+                    css={css`
+                      margin-top: 100px;
+                      text-align: center;
+                    `}
+                  >
+                    Loading...
+                  </div>
+                }
+              >
+                <AskPage />
+              </React.Suspense>
+            }
+          />
+          <Route path="signin" element={<SignInPage />} />
+          <Route path="questions/:questionId" element={<QuestionPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
